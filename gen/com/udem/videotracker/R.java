17c29a2816f9b94f4d0918873e169d8aee647949
Romain Piche <rpiche@polytech.unice.fr>
2014-04-09 11:07:11 -0400

    pb avec classpath et lib et pb avec un fichier generé résolu

--- gen/com/udem/videotracker/R.java
+++ gen/com/udem/videotracker/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -24,36 +29,50 @@ public final class R {
         public static final int ic_action_collections_sort_by_size=0x7f020000;
         public static final int ic_action_images_slideshow=0x7f020001;
         public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int playlist=0x7f020004;
+        public static final int ic_av_play=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int mqdefault=0x7f020005;
+        public static final int playlist=0x7f020006;
     }
     public static final class id {
-        public static final int button_search=0x7f080001;
-        public static final int menu_a_propos=0x7f080009;
-        public static final int menu_add=0x7f08000c;
-        public static final int menu_aide=0x7f080008;
-        public static final int menu_playlist=0x7f08000a;
-        public static final int menu_preferences=0x7f080007;
-        public static final int menu_search=0x7f080003;
-        public static final int menu_share=0x7f08000b;
-        public static final int search_query=0x7f080002;
-        public static final int text_search=0x7f080000;
-        public static final int tri_alpha=0x7f080005;
-        public static final int tri_date=0x7f080004;
-        public static final int tri_nombre=0x7f080006;
+        public static final int action_settings=0x7f080016;
+        public static final int button_search=0x7f080003;
+        public static final int checkbox_dailymotion=0x7f080005;
+        public static final int checkbox_fav=0x7f080000;
+        public static final int checkbox_youtube=0x7f080004;
+        public static final int menu_a_propos=0x7f080012;
+        public static final int menu_add=0x7f080015;
+        public static final int menu_aide=0x7f080011;
+        public static final int menu_playlist=0x7f080013;
+        public static final int menu_preferences=0x7f080010;
+        public static final int menu_search=0x7f08000c;
+        public static final int menu_share=0x7f080014;
+        public static final int search_query=0x7f080006;
+        public static final int test=0x7f080001;
+        public static final int text_search=0x7f080002;
+        public static final int tri_alpha=0x7f08000e;
+        public static final int tri_date=0x7f08000d;
+        public static final int tri_nombre=0x7f08000f;
+        public static final int video_auteur=0x7f080008;
+        public static final int video_description_complet=0x7f08000a;
+        public static final int video_nbVues=0x7f080009;
+        public static final int video_titre=0x7f080007;
+        public static final int video_url=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_aide=0x7f030000;
         public static final int activity_pref=0x7f030001;
         public static final int activity_propos=0x7f030002;
         public static final int activity_recherche=0x7f030003;
-        public static final int activity_video=0x7f030004;
+        public static final int activity_resultat_video=0x7f030004;
+        public static final int activity_video=0x7f030005;
     }
     public static final class menu {
         public static final int menu_playlists=0x7f070000;
         public static final int menu_recherche=0x7f070001;
         public static final int menu_video=0x7f070002;
         public static final int menu_videos=0x7f070003;
+        public static final int video=0x7f070004;
     }
     public static final class string {
         public static final int a_propos_activity_text=0x7f05000b;
@@ -61,7 +80,10 @@ public final class R {
         public static final int a_propos_version_text=0x7f05000c;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int checkbox_dailymotion=0x7f050013;
+        public static final int checkbox_youtube=0x7f050012;
         public static final int description_image_recherche=0x7f05000a;
+        public static final int hello_world=0x7f050015;
         public static final int menu_a_propos=0x7f050005;
         public static final int menu_add=0x7f050009;
         public static final int menu_aide=0x7f050004;
@@ -71,6 +93,7 @@ public final class R {
         public static final int menu_share=0x7f050008;
         public static final int menu_trier=0x7f050007;
         public static final int search_query=0x7f050011;
+        public static final int title_activity_video=0x7f050014;
         public static final int tri_alpha=0x7f05000f;
         public static final int tri_date=0x7f05000e;
         public static final int tri_nombre=0x7f050010;
