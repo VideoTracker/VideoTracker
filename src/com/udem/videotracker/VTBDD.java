4ce6f284139db2ec374ce2394c9514516fea7c48
Hugo Martinez <hugo.83300@gmail.com>
2014-04-29 18:53:01 -0400

    Ajout des fonctions de bdd

--- src/com/udem/videotracker/VTBDD.java
+++ src/com/udem/videotracker/VTBDD.java
@@ -1,8 +1,15 @@
 package com.udem.videotracker;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.udem.videotracker.VideoAdapter.VideoData;
+
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 
 
@@ -22,20 +29,11 @@ public class VTBDD {
 	private static final String V_DATEPUB = "date_publication";
 	private static final String V_THUMBMAIL = "thumbmail";
 	
-	private static final String CREATE_VIDEO = "CREATE TABLE " + TABLE_VIDEO + " ("
-			+ V_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + V_TITLE + " TEXT NOT NULL, "
-			+ V_DESC + " TEXT NOT NULL, " + V_LINK + " TEXT NOT NULL, " + V_DATEPUB + " TEXT NOT NULL, "
-			+ V_THUMBMAIL + " TEXT NOT NULL);";
-	
-	
 	private static final String TABLE_PLAYLIST = "table_playlist";
 	private static final String COL_ID = "id_playlist";
 	private static final String COL_PNAME = "name";
 	private static final String COL_DATE = "date";
  
-	private static final String CREATE_PLAYLIST = "CREATE TABLE " + TABLE_PLAYLIST + " ("
-	+ COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_PNAME + " TEXT NOT NULL, "
-	+ COL_DATE + " TEXT NOT NULL);";
 	
 	private static final String TABLE_ASSOCIATION = "Association";
 	/*
@@ -43,9 +41,7 @@ public class VTBDD {
 	 */
 	private static final String A_IDVIDEO = "id_video";
 	private static final String A_IDPLAYLIST = "id_playlist";
-	
-	private static final String CREATE_ASSOCIATION = "CREATE TABLE " + TABLE_PLAYLIST + " ("+ A_IDVIDEO + " INTEGER NOT NULL, "
-			+ A_IDPLAYLIST + " INTEGER NOT NULL);";
+
 	
 	private static final String TABLE_HISTORIQUE = "Historique";
 	/*
@@ -54,7 +50,6 @@ public class VTBDD {
 	private static final String H_ID = "id_historique";
 	private static final String H_KEYWORDS = "keywords";
 	
-	private static final String CREATE_HISTORIQUE = "CREATE TABLE " + TABLE_HISTORIQUE + " ("+H_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT, "+ H_KEYWORDS + " TEXT NOT NULL);";
 	/*
 	 * On utilisera ce contexte pour afficher des toasts.
 	 */
@@ -93,7 +88,7 @@ public class VTBDD {
 	    bdd.insert(TABLE_PLAYLIST, null, values);		
 	}
 	
-	public void addToVideo(VideoAdapter.VideoData data, SQLiteDatabase db){
+	public long addToVideo(VideoAdapter.VideoData data){
 		ContentValues values = new ContentValues();
 		values.put(V_TITLE, data.title);
 		values.put(V_DESC, data.description);
@@ -101,7 +96,58 @@ public class VTBDD {
 		values.put(V_DATEPUB, data.datePublication.toString());
 		values.put(V_THUMBMAIL, data.url_image);
 		
-		bdd.insertOrThrow(TABLE_VIDEO,null, values);
+		return bdd.insertOrThrow(TABLE_VIDEO,null, values);
+		
+	}
+	
+	public ArrayList<VideoAdapter.VideoData> getPlaylistContent(int id){
+		
+		ArrayList<VideoAdapter.VideoData> ret = new ArrayList<VideoAdapter.VideoData>();
+		
+	    // 2. build query
+	    Cursor cursor = 
+	            bdd.query(TABLE_ASSOCIATION, // a. table
+	           new String[]{ A_IDVIDEO,A_IDPLAYLIST}, // b. column names
+	            A_IDPLAYLIST+"= ?", // c. selections 
+	            new String[] { String.valueOf(id) }, // d. selections args
+	            null, // e. group by
+	            null, // f. having
+	            null, // g. order by
+	            null); // h. limit
+	 
+	    // 3. if we got results get the first one
+	    if (cursor != null){
+	    	while(cursor.moveToNext()){
+	    		ret.add(getVideo(cursor.getInt(0)));
+	    	}
+	    }
+	 
+	 
+	    // 5. return book
+	    return ret;
+	}
+	
+	
+	public VideoAdapter.VideoData getVideo(int id){
+		
+		   // 2. build query
+	    Cursor cursor = 
+	            bdd.query(TABLE_VIDEO, // a. table
+	           new String[]{ V_ID,V_TITLE, V_DESC, V_LINK, V_DATEPUB, V_THUMBMAIL}, // b. column names
+	            V_ID+"= ?", // c. selections 
+	            new String[] { String.valueOf(id) }, // d. selections args
+	            null, // e. group by
+	            null, // f. having
+	            null, // g. order by
+	            null); // h. limit
+	    
+	    if (cursor != null){
+	    	cursor.moveToNext();
+	    }
+	    
+	    @SuppressWarnings("deprecation")
+		VideoAdapter.VideoData ret = new VideoAdapter.VideoData(cursor.getString(1), cursor.getString(2), "", cursor.getString(3), cursor.getString(5), false, false, 0, 0, null, new Date(cursor.getString(4)), null);
+		return ret;
 		
 	}
 	
@@ -110,12 +156,42 @@ public class VTBDD {
 			
 	}
 	
+	public boolean deleteVideoFromPlaylist(int id_video, int id_playlist){
+		return bdd.delete(TABLE_ASSOCIATION, A_IDPLAYLIST + "=" + id_playlist + " AND " + A_IDVIDEO + "=" + id_video  , null) > 0;
+	}
+	
+	public boolean addVideoToPlaylist(VideoAdapter.VideoData vid, int id_playlist){
+		int id_video = (int) addToVideo(vid);
+		ContentValues values = new ContentValues();
+		values.put(A_IDVIDEO, id_video);
+		values.put(A_IDPLAYLIST, id_playlist);
+		bdd.insertOrThrow(TABLE_ASSOCIATION, null, values);
+		return true;
+	}
 	public void addToHistoric(String keywords) {
 		
 		ContentValues values = new ContentValues();
 		values.put(H_KEYWORDS, keywords);
 		
 		bdd.insertOrThrow(TABLE_HISTORIQUE, null, values);
+		
+	}
+	
+	public ArrayList<String> getHistoric(){
+		int count = 0;
+		ArrayList<String> ret = new ArrayList<String>();
+		String query = "Select * FROM" + TABLE_HISTORIQUE;
+		Cursor curs = bdd.rawQuery(query, null);
+		
+		if(curs.moveToFirst()){
+			do{
+				ret.add(curs.getString(1));
+				count++;
+			}while(curs.moveToNext() && count <5);
+			
+		}
+		
+		return ret;
 		
 	}
 	
