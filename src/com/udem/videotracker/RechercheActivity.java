351bcb1ca2d203390f12c1a5dd3558be9692f5a0
Anthony Da Mota <thony-83@hotmail.fr>
2014-04-29 15:08:39 -0500

    fgf

--- src/com/udem/videotracker/RechercheActivity.java
+++ src/com/udem/videotracker/RechercheActivity.java
@@ -17,7 +17,6 @@ import android.view.View.OnClickListener;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 /**
